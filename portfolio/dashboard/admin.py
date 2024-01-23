dfdc0360dc90f5c30170927d69c604f2566b8ebe
Chido100 <chido_dozie@hotmail.com>
2024-01-23 14:14:33 +0000

    Registered apps in the admin

--- portfolio/dashboard/admin.py
+++ portfolio/dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dashboard, Experience, Project, About
 
-# Register your models here.
+admin.site.register(Dashboard)
+admin.site.register(Experience)
+admin.site.register(Project)
+admin.site.register(About)
