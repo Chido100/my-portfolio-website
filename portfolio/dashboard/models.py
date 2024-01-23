25ac2c6efe0727314415769f339f4a17a0b4b09a
Chido100 <chido_dozie@hotmail.com>
2024-01-23 14:06:19 +0000

    Added all apps models

---
+++ portfolio/dashboard/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+import PIL
+
+
+
+class Dashboard(models.Model):
+    name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='profile_image')
+
+    def __str__(self):
+        return self.name
+
+
+class Experience(models.Model):
+    job_title = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    duration = models.CharField(max_length=50)
+    duty = models.TextField()
+
+    def __str__(self):
+        return self.company_name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class About(models.Model):
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.summary
+
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'dashboard',
+    
+
     'crispy_forms',
     'crispy_bootstrap5',
 ]
